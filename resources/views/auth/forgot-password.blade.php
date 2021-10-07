7e61513ca4932379d4b66f0f08c8d8bf2bfddeec
Olivia Hales <halesolivia7@gamil.com>
2021-10-07 18:31:48 -0500

    Login and signup updates

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,31 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+  <div class="relative min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover relative items-center"
+  style="background-image: url(https://images.unsplash.com/photo-1525302220185-c387a117886e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80);">
 
-        <div class="mb-4 text-sm text-gray-600">
+  <div class="bg-white p-6 m-10 rounded-lg border-solid w-2/6">
+     <div class="mb-4 text-sm text-gray-600">
             {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
         </div>
 
-        <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
 
-            <!-- Email Address -->
             <div>
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="w-full text-base py-2 border-b outline-none" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <x-button>
+                <x-button class="mt-4 bg-indigo-500 text-gray-100 p-2  rounded-full tracking-wide font-semibold  focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300">
                     {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
         </form>
-    </x-auth-card>
+      </div>
+  </div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,85 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+  <div class="flex right-1 top-1  flex-row-reverse lg:p-1 lg:top-2 lg:right-2 absolute z-10 bg-gray-200 ">
+    <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
+        {{__('SignUp')}}
+    </x-responsive-nav-link>
+  
+    <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
+      {{__('Login')}}
+  </x-responsive-nav-link>
+  </div>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+  <div class="min-h-screen flex justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover relative items-center"
+  style="background-image: url(https://images.unsplash.com/photo-1525302220185-c387a117886e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80);">
+  <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+     <div class="max-w-md w-full space-y-8 p-10 bg-white rounded-xl z-10">
+                
+      <div class="text-center">
+        <h2 class="mt-2 text-3xl font-bold text-gray-900">
+          Sign Up
+        </h2>
+             
+        <p class="mt-2 text-sm text-gray-600">Please sign up for your account</p>
+      </div>
+         <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                
+         <form class="mt-8 space-y-6" method="POST" action="{{route('register')}}">
+            @csrf            
+            <input type="hidden" name="remember" value="true">
+              <div class="relative">
+                  <div class="absolute right-0 mt-4">
+                      {{-- <svg xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                      </svg> --}}
+                    </div>
+                    <div>
+                     <label class="text-sm font-bold text-gray-700 tracking-wide">Name</label>
+                     <x-input id="name" class="w-full text-base py-2 border-b" type="text" name="name" :value="old('name')" required autofocus />
+                   </div>
+                        {{-- @if ('email')
+                    {{message}}        
+                    @endif --}} 
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+                    <div class="mt-2 content-center">
+                      <label class="text-sm font-bold text-gray-700 tracking-wide">Email</label>
+                      <x-input id="email" class="block mt-1 w-full border-b" type="email" name="email" :value="old('email')" required />
+                  </div>
+                  {{-- @if ('password')
+                      {{message}}
+                    @endif --}}
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                  <div class="mt-2 content-center">
+                    <label class="text-sm font-bold text-gray-700 tracking-wide">Password</label>
+                    <x-input id="password" class="block mt-1 w-full border-b" type="password" name="password" required autocomplete="new-password"/> 
+                    {{-- @if ('password')
+                      {{message}}
+                    @endif --}}
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                  </div>
+                  <div class="mt-2 content-center">
+                    <label class="text-sm font-bold text-gray-700 tracking-wide">Confirm Password</label>
+                    <x-input id="password_confirmation" class="block mt-1 w-full border-b" type="password" name="password_confirmation" required />
+                  </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+                  </div>
+                  <div class="flex items-center justify-between">
+                          <x-button class="w-full flex justify-center bg-indigo-500 text-gray-100 p-4 rounded-full tracking-wide
+                            font-semibold  focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300">
+                            {{ __('Register') }}
+                          </x-button>
+                  </div>
+                  <div class="text-sm">
+                    @if (Route::has('password.request'))
+                      <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{route('login')}}">
+                        {{__('Already registered?')}}
+                        </a>
+                    @endif
+                  </div>
+                </form>
+              </div>
+    </div>
+ </x-guest-layout>
