34e327733d99fb3bf32a323a51bbba29d026f2a7
Olivia Hales <halesolivia7@gamil.com>
2021-10-08 09:10:35 -0500

    form update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{__('Dashboard')}}
         </h2>
+        <h2 class="font-semibold text-sm bg-red-500 rounded-md p-2">{{__('Log Out')}}</h2>
     </x-slot>
 
     <div class="py-12">
--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -1,12 +1,10 @@
 <x-app-layout>
 
-    <x-slot name="header">
-
-    </x-slot>
-
-    
-
-
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Dashboard') }}
+    </h2>
+</x-slot>
 
 <div class="flex items-center justify-center min-h-screen w-full">
 <div>
